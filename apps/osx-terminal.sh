2567406e7e89333a1d953fc6e5971e14c72db1f7
Ryan Hansohn <ryan.hansohn@gmail.com>
2015-09-01 09:24:32 -0700

    update script file permissions and add echos

--- apps/osx-terminal.sh
+++ apps/osx-terminal.sh
@@ -8,6 +8,7 @@ source ${SCRIPTPATH}/_config.sh;
 
 # install solarized.clr color palette
 if ! -f ~/Library/Colors/solarized.clr; then
+  echo "==> Instaling solarized color palette";
   wget -O ~/Library/Colors/solarized.clr https://raw.githubusercontent.com/altercation/solarized/master/apple-colorpalette-solarized/solarized.clr;
 fi
 
@@ -15,12 +16,14 @@ fi
 if ! defaults read ~/Library/Preferences/com.apple.Terminal.plist | grep -q -E "(solarized-light|solarized-dark)"; then
   # install solarized-dark osx terminal color profile
   if ! -f /tmp/solarized-dark.terminal ; then
+    echo "==> Instaling solarized-dark osx terminal profile";
     wget -O /tmp/solarized-dark.terminal https://raw.githubusercontent.com/tomislav/osx-terminal.app-colors-solarized/master/Solarized%20Dark.terminal;
     open --background -a Terminal /tmp/solarized-dark.terminal;
   fi
 
   # install solarized-light osx terminal color profile
   if ! -f /tmp/solarized-light.terminal ; then
+    echo "==> Instaling solarized-light osx terminal profile";
     wget -O /tmp/solarized-light.terminal https://raw.githubusercontent.com/tomislav/osx-terminal.app-colors-solarized/master/Solarized%20Light.terminal;
     open --background -a Terminal /tmp/solarized-light.terminal;
   fi
@@ -28,6 +31,7 @@ fi
 
 # set profile in terminal as default
 if [[ ! `defaults read ~/Library/Preferences/com.apple.Terminal.plist "Default Window Settings"` == "solarized-dark" ]]; then
+  echo "==> Setting solarized-dark as default color profile in osx terminal";
   defaults write ~/Library/Preferences/com.apple.Terminal.plist "Default Window Settings" "solarized-dark";
   defaults write ~/Library/Preferences/com.apple.Terminal.plist "Startup Window Settings" "solarized-dark";
 fi
