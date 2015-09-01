cdbf43bb8f98d9758c4562c0bb9822e1f4a8c7b5
Ryan Hansohn <hansohn@users.noreply.github.com>
2015-08-31 22:40:55 -0700

    remove sudo grace periods. they dont work properly

--- apps/install.sh
+++ apps/install.sh
@@ -21,9 +21,5 @@ apps=(
 
 # install selected apps
 for app in ${apps[@]}; do
-  # install app
   source `dirname "${BASH_SOURCE[0]}"`/${app}.sh;
-
-  # reinitialize sudo grace period
-  sudo -v;
 done
