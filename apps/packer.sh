12dde0bbff18c9b6b4524487fe766c5b304851d0
Ryan Hansohn <hansohn@users.noreply.github.com>
2015-08-31 21:44:49 -0700

    remove packer windows plugins install. they are included with v 0.8.x by default

--- apps/packer.sh
+++ apps/packer.sh
@@ -10,25 +10,11 @@ source ${SCRIPTPATH}/_config.sh;
 source ${SCRIPTPATH}/homebrew.sh;
 source ${SCRIPTPATH}/wget.sh;
 
-# variables
-PACKER_DIR="`readlink $(which packer) | sed 's/\/packer//2'`"
-PACKER_WIN_PLGNS="https://github.com/packer-community/packer-windows-plugins/releases/download/v1.0.0-rc/darwin_amd64.zip"
 
 # Install Packer
 if ! brew cask ls | grep "^packer$" > /dev/null 2>&1; then
     echo "==> Installing Packer";
-    sudo brew cask install packer;
-fi
-
-# Install Packer-Windows-Plugins
-if brew cask ls | grep "^packer$" > /dev/null 2>&1; then
-  if ! ls -Glh ${PACKER_DIR} | grep "\-windows\-" > /dev/null 2>&1; then
-    echo "==> Installing Packer-Windows-Plugins"
-    sudo curl -L ${PACKER_WIN_PLGNS} > /tmp/darwin_amd64.zip;
-    sudo unzip -x /tmp/darwin_amd64.zip -d ${PACKER_DIR};
-    sudo chown -R root:wheel ${PACKER_DIR}/*
-    sudo rm -f /tmp/darwin_amd64.zip;
-  fi
+    brew cask install packer;
 fi
 
 # install packer-completions
