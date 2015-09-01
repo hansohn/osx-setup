bf4ec438937deb552a2cbf5d03d1f19d3a5e8f14
Ryan Hansohn <hansohn@users.noreply.github.com>
2015-08-31 22:48:39 -0700

    fix git merge develop issue

--- apps/bash.sh
+++ apps/bash.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-<<<<<<< HEAD
-# import config vars
-source ./_config.sh;
-
-# install prerequisites
-source ./homebrew.sh;
-
-# install bash
-if ! brew ls | grep '^bash$' > /dev/null 2>&1; then
-  echo "==> Installing Bash";
-  brew install bash;
-=======
 # set vars
 SCRIPTPATH=`dirname "${BASH_SOURCE[0]}"`;
 
@@ -30,7 +18,6 @@ fi
 # install bash-completions
 if ! brew ls | grep -i -q "bash-completion"; then
   echo "==> Installing Bash-Completion";
->>>>>>> develop
   brew install bash-completion;
 fi
 
