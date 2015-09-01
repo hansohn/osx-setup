ebb59fe1db38593c72fe6a5023c1cf2de08ca036
Ryan Hansohn <hansohn@users.noreply.github.com>
2015-08-31 21:45:59 -0700

    remove sudo from install command

--- apps/virtualbox.sh
+++ apps/virtualbox.sh
@@ -12,5 +12,5 @@ source ${SCRIPTPATH}/homebrew.sh;
 # install virtualbox
 if ! brew cask ls | grep "^virtualbox$" > /dev/null 2>&1; then
   echo "==> Installing VirtualBox"
-  sudo brew cask install virtualbox;
+  brew cask install virtualbox;
 fi
