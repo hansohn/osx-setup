d9f745fd0d2491e98ae4e94ed01126075dd21358
Ryan Hansohn <rhansohn@payoff.com>
2015-09-12 17:52:06 -0700

    modify config.sh name and location

--- config.sh
+++ config.sh
@@ -5,6 +5,14 @@ FULL_NAME="";
 EMAIL_ADDRESS="";
 PHONE_NUMBER="";
 
+# ----- osx -----
+SCREENSHOT_LOC="/Users/${USER}/Pictures/Screenshots";
+TIMEZONE="America/Los_Angeles";
+
+# ----- git config -----
+GIT_USER_NAME="";
+GIT_USER_EMAIL="";
+
 # ----- switches -----
 WINDOWS_DEV=true;
 iterm_install=false;
