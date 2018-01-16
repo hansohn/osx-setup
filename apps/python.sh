ec64c48331dcf0d0d641070c10c478587cb0fb61
Ryan Hansohn <hansohn@users.noreply.github.com>
2018-01-16 09:20:04 -0800

    fix bugs. add meld

--- apps/python.sh
+++ apps/python.sh
@@ -10,15 +10,15 @@ source ${SCRIPTPATH}/../config.sh;
 source ${SCRIPTPATH}/homebrew.sh;
 
 # install python2
-if ! brew cask ls | grep "^python$" > /dev/null 2>&1; then
+if ! brew ls | grep "^python$" > /dev/null 2>&1; then
   echo "==> Installing Python2";
-  brew cask install python;
+  brew install python;
   pip2 install --upgrade pip setuptools;
 fi
 
 # install python3
-if ! brew cask ls | grep "^python3$" > /dev/null 2>&1; then
+if ! brew ls | grep "^python3$" > /dev/null 2>&1; then
   echo "==> Installing Python3";
-  brew cask install python3;
+  brew install python3;
   pip3 install --upgrade pip setuptools;
 fi
