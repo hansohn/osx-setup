e25bace7a49e34da2ae088920daf45bee406d801
Ryan Hansohn <hansohn@users.noreply.github.com>
2018-09-20 13:20:19 -0700

    update docker bash completion install

--- apps/docker.sh
+++ apps/docker.sh
@@ -17,14 +17,11 @@ fi
 
 # install docker-completion
 docker_bashcomps=(
-  "docker"
-  "docker-machine"
-  "docker-compose"
+  "docker-completion"
 );
 for docker_bashcomp in ${docker_bashcomps[@]}; do
   if [ ! -f "/usr/local/etc/bash_completion.d/${docker_bashcomp}" ]; then
     echo "==> Linking Docker Bash Completion: ${docker_bashcomp}";
-    ln -s "/Applications/Docker.app/Contents/Resources/etc/${docker_bashcomp}.bash-completion" \
-          "/usr/local/etc/bash_completion.d/${docker_bashcomp}";
+    brew install docker-completion;
   fi
 done
