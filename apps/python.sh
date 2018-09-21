97a37a0dcd03e590cc65417e3a57a983c2069ddd
Ryan Hansohn <hansohn@users.noreply.github.com>
2018-09-21 15:49:52 -0700

    fix python2 install

--- apps/python.sh
+++ apps/python.sh
@@ -12,13 +12,13 @@ source ${SCRIPTPATH}/homebrew.sh;
 # install python2
 if ! brew ls | grep "^python@2$" > /dev/null 2>&1; then
   echo "==> Installing Python2";
-  brew install python;
+  brew install python@2;
   python2 -m pip install --upgrade pip setuptools wheel;
 fi
 
 # install python3
 if ! brew ls | grep "^python$" > /dev/null 2>&1; then
   echo "==> Installing Python3";
-  brew install python3;
+  brew install python;
   python3 -m pip install --upgrade pip setuptools wheel;
 fi
