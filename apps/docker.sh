6d3dc7e18ab035e6228b48c5222a1e9cd69689a7
Ryan Hansohn <hansohn@users.noreply.github.com>
2018-09-21 15:49:26 -0700

    fix docker-completion install

--- apps/docker.sh
+++ apps/docker.sh
@@ -16,12 +16,7 @@ if ! brew cask ls | grep "^docker$" > /dev/null 2>&1; then
 fi
 
 # install docker-completion
-docker_bashcomps=(
-  "docker-completion"
-);
-for docker_bashcomp in ${docker_bashcomps[@]}; do
-  if [ ! -f "/usr/local/etc/bash_completion.d/${docker_bashcomp}" ]; then
-    echo "==> Linking Docker Bash Completion: ${docker_bashcomp}";
-    brew install docker-completion;
-  fi
-done
+if ! brew ls | grep "^docker-completion$" /dev/null 2>&1; then
+  echo "==> Install Docker-Completion";
+  brew install docker-completion;
+fi
