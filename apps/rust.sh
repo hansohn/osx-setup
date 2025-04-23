62c982d701093b52855df060b24dde0bff095c93
Ryan Hansohn <hansohn@users.noreply.github.com>
2025-04-23 09:49:10 -0700

    align with current

---
+++ apps/rust.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# set vars
+SCRIPTPATH=$(dirname "${BASH_SOURCE[0]}");
+RUSTUP_INSTALL_NIGHTLY="true"
+
+# import config vars
+source "${SCRIPTPATH}/../config.sh";
+
+# install rustup
+if ! which rustc > /dev/null 2>&1; then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  export PATH="${HOME}/.cargo/bin:${PATH}"
+
+  # install rust nightly
+  if [ "${RUSTUP_INSTALL_NIGHTLY}" == "true" ] && which rustup > dev/null 2>&1; then
+    rustup toolchain install nightly
+    rustup default nightly
+  fi
+fi
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # set vars
-BOOTSCRIPT_PATH=$(dirname "${BASH_SOURCE[0]}");
+BOOTSCRIPT_PATH=$(dirname "${BASH_SOURCE[0]}")
 
 # import config vars
-source "${BOOTSCRIPT_PATH}/config.sh";
+source "${BOOTSCRIPT_PATH}/config.sh"
 
 # ----- dotfiles -----
 
-dotfile_dir="${HOME}/.dotfiles";
-backup_dir="${dotfile_dir}/bak/$(date +%Y%m%d)";
-vim_dir="${HOME}/.vim";
+dotfile_dir="${HOME}/.dotfiles"
+backup_dir="${dotfile_dir}/bak/$(date +%Y%m%d)"
+vim_dir="${HOME}/.vim"
 
 dotfiles=(
   '.bash_profile'
@@ -19,39 +19,39 @@ dotfiles=(
   '.vimrc'
   '.vim/plugins.vim'
   '.zshrc'
-);
+)
 
 # create dirs
 for dir in "${dotfile_dir}" "${backup_dir}" "${vim_dir}"; do
   if [ ! -d "${dir}" ]; then
-    echo "==> Creating directory: ${dir}";
-    mkdir -p "${dir}";
+    echo "==> Creating directory: ${dir}"
+    mkdir -p "${dir}"
   fi
 done
 
 # copy dotfile contents
-echo "==> Populating dotfile directory";
-rsync -ah "${BOOTSCRIPT_PATH}/dotfiles/" "${dotfile_dir}/";
+echo "==> Populating dotfile directory"
+rsync -ah "${BOOTSCRIPT_PATH}/dotfiles/" "${dotfile_dir}/"
 
 # manage dotfile
 for dotfile in "${dotfiles[@]}"; do
   # backup if not symlink
-  if [ -f "${HOME}/${dotfile}" ] && [ ! -L  "${HOME}/${dotfile}" ]; then
-    echo "==> Archiving: ${HOME}/${dotfile} to ${backup_dir}";
-    mv "${HOME}/${dotfile}" "${backup_dir}/${dotfile}";
+  if [ -f "${HOME}/${dotfile}" ] && [ ! -L "${HOME}/${dotfile}" ]; then
+    echo "==> Archiving: ${HOME}/${dotfile} to ${backup_dir}"
+    mv "${HOME}/${dotfile}" "${backup_dir}/${dotfile}"
   fi
 
   # symlink
   if [ ! -L "${HOME}/${dotfile}" ]; then
-    echo "==> Linking: ${dotfile_dir}/${dotfile} to ${HOME}/${dotfile}"; 
-    ln -s "${dotfile_dir}/${dotfile}" "${HOME}/${dotfile}";
+    echo "==> Linking: ${dotfile_dir}/${dotfile} to ${HOME}/${dotfile}"
+    ln -s "${dotfile_dir}/${dotfile}" "${HOME}/${dotfile}"
   fi
 done
 
 # ----- personalization -----
 
-source "${BOOTSCRIPT_PATH}/customizations/system-settings.sh";
-source "${BOOTSCRIPT_PATH}/customizations/user-settings.sh";
+source "${BOOTSCRIPT_PATH}/customizations/system-settings.sh"
+source "${BOOTSCRIPT_PATH}/customizations/user-settings.sh"
 
 # ----- apps -----
 
@@ -66,22 +66,24 @@ apps=(
   "osx-terminal"
   "packer"
   "python"
+  "rust"
   "terraform"
   "vagrant"
   "vim"
-);
+)
 
-# list cask apps for basic install
+# list of homebrew formulae to install
 brew_formulae=(
   "ansible"
   "ansible-lint"
+  "argocd"
   "aws-vault"
   "awscli"
+  "bat"
   "cfn-lint"
   "curl"
   "doctl"
   "fd"
-  "font-hack-nerd-font"
   "fzf"
   "gh"
   "gnupg"
@@ -90,18 +92,19 @@ brew_formulae=(
   "grep"
   "helm"
   "ipcalc"
+  "istioctl"
   "jq"
   "jsonlint"
   "k9s"
+  "kind"
   "kubernetes-cli"
+  "kustomize"
   "lazygit"
   "mas"
   "neovim"
   "nmap"
   "ripgrep"
   "shellcheck"
-  "teamookla/speedtest/speedtest"
-  "terraform"
   "terraform-docs"
   "terraform-ls"
   "terragrunt"
@@ -112,22 +115,21 @@ brew_formulae=(
   "warrensbox/tap/tfswitch"
   "wget"
   "yamllint"
-);
+  "zsh"
+)
 
-# list cask apps for basic install
+# list of homebrew casks to install
 brew_casks=(
   "1password"
   "adobe-acrobat-reader"
   "appcleaner"
   "brooklyn"
-  "burp-suite"
   "discord"
   "docker"
   "firefox"
+  "font-hack-nerd-font"
   "google-chrome"
-  "google-cloud-sdk"
   "google-drive"
-  "keybase"
   "obsidian"
   "postman"
   "raycast"
@@ -135,46 +137,45 @@ brew_casks=(
   "session-manager-plugin"
   "slack"
   "spotify"
-  "ubiquiti-unifi-controller"
-  "virtualbox"
-  "virtualbox-extension-pack"
+  "virtualbox@beta"
   "visual-studio-code"
   "wireshark"
   "zoom"
-);
+)
 
 # mas apps
 mas_apps=(
-  "937984704"     # Amphetamine
-  "1292198261"    # iMazing HEIC Converter
-  "1033480833"    # Decompressor
+  "1033480833" # Decompressor
+  "1292198261" # iMazing HEIC Converter
+  "302584613"  # Kindle
+  "937984704"  # Amphetamine
 )
 
 # install selected custom apps
 # shellcheck disable=SC1090
 for app in "${apps[@]}"; do
-  source "${BOOTSCRIPT_PATH}/apps/${app}.sh";
+  source "${BOOTSCRIPT_PATH}/apps/${app}.sh"
 done
 
 # install selected brew apps
 for formula in "${brew_formulae[@]}"; do
-  if ! brew ls | grep "^${formula}$" > /dev/null 2>&1; then
-    echo "==> Installing ${formula}";
-    brew install "${formula}";
+  if ! brew ls | grep "^${formula}$" >/dev/null 2>&1; then
+    echo "==> Installing ${formula}"
+    brew install "${formula}"
   fi
 done
 
 # install selected cask apps
 for cask in "${brew_casks[@]}"; do
-  if ! brew ls | grep "^${cask}$" > /dev/null 2>&1; then
-    echo "==> Installing ${cask}";
-    brew install --cask "${cask}";
+  if ! brew ls | grep "^${cask}$" >/dev/null 2>&1; then
+    echo "==> Installing ${cask}"
+    brew install --cask "${cask}"
   fi
 done
 
 # install mas apps
 for app in "${mas_apps[@]}"; do
-  if ! mas list | grep -e "^${app}$" > /dev/null 2>&1; then
-    mas install "${app}";
+  if ! mas list | grep -e "^${app}$" >/dev/null 2>&1; then
+    mas install "${app}"
   fi
 done
